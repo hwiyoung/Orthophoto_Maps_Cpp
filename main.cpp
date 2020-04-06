51fea14d4c8d34dd54119b9baee7cf9973d73764
hwiyoung <huddy1204@gmail.com>
2020-04-06 16:14:45 +0900

    feat: Include opencv, exiv2, gdal

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include <exiv2/exiv2.hpp>
+#include "gdal_priv.h"
+
+using namespace std;
+using namespace cv;
+using namespace Exiv2;
+
+int main() {
+    cout << "Hello, World!" << endl;
+    return 0;
+}
